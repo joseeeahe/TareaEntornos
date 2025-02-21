962d8d90109bad40fa31368af0fe1dd12acba78b
José Antonio Herrera Expósito <joseantonioherreraexposito@gmail.com>
2025-02-21 09:01:38 +0000

    Subiendo código fuente y pruebas iniciales

---
+++ githubdos.py
@@ -0,0 +1,13 @@
+# ejercicio para la práctica de empresa...
+import os
+os.system('cls')
+
+"""
+DECIR SI UN NUMERO ES PAR O NO
+"""
+num = int(input("Ingresa un número: "))
+
+if num % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
