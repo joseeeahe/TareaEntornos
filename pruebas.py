a95d724b48cee916b2f207461b5bfc943962fc95
José Antonio Herrera Expósito <joseantonioherreraexposito@gmail.com>
2025-02-21 10:21:27 +0000

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,20 @@
+# este archivo lo hemos creado para las pruebas:
+
+
+
+
+# Prueba la función
+from github import resta, suma
+
+
+resultado = suma(5, 3)
+if resultado == 8:
+    print("La función funciona correctamente")
+else:
+    print("ERROR: La función no está funcionando bien")
+
+resultadov2 = resta(8,2)
+if resultadov2 == 10:
+    print("No funciona bien")
+else:
+    print("funciona bien...")
